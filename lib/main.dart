246d66477178af0633998c8b6c2f041465e26c53
ahmadkh1509-cyber <ahmadkh1509@gmail.com>
2026-08-06 23:30:32 +0500

    new thngs like grid time format etc

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
+import 'package:intl/intl.dart';
 
 void main() {
   runApp(const MyApp());
@@ -45,16 +46,17 @@ class HomeScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     var email = TextEditingController();
 
-    var arrName = [
-      'ahmad',
-      'ali',
-      'zubair ',
-      'subtain',
-      'falak',
-      'molvi',
-      'tania',
-      'tashu',
-    ];
+    // var arrName = [
+    //   'ahmad',
+    //   'ali',
+    //   'zubair ',
+    //   'subtain',
+    //   'falak',
+    //   'molvi',
+    //   'tania',
+    //   'tashu',
+    // ];
+    var Time = DateTime.now();
     return Scaffold(
       appBar: AppBar(
         backgroundColor: Colors.green,
@@ -391,62 +393,106 @@ class HomeScreen extends StatelessWidget {
       //     ),
       //   ),
       // ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            Container(
-              width: 300,
-              child: TextField(
-                controller: email,
-                obscureText: true,
-                decoration: InputDecoration(
-                  hint: Text(
-                    'INPUT YOU EMAIL HERE',
-                    style: TextStyle(
-                      color: Colors.green,
-                      fontWeight: FontWeight.bold,
-                    ),
-                  ),
-                  focusedBorder: OutlineInputBorder(
-                    borderRadius: BorderRadius.circular(14),
-                    borderSide: BorderSide(
-                      color: const Color.fromARGB(255, 38, 5, 96),
-                      width: 2,
-                    ),
-                  ),
+      //   body: Center(
+      //     child: Column(
+      //       mainAxisAlignment: MainAxisAlignment.center,
+      //       children: [
+      //         Container(
+      //           width: 300,
+      //           child: TextField(
+      //             controller: email,
+      //             obscureText: true,
+      //             decoration: InputDecoration(
+      //               hint: Text(
+      //                 'INPUT YOU EMAIL HERE',
+      //                 style: TextStyle(
+      //                   color: Colors.green,
+      //                   fontWeight: FontWeight.bold,
+      //                 ),
+      //               ),
+      //               focusedBorder: OutlineInputBorder(
+      //                 borderRadius: BorderRadius.circular(14),
+      //                 borderSide: BorderSide(
+      //                   color: const Color.fromARGB(255, 38, 5, 96),
+      //                   width: 2,
+      //                 ),
+      //               ),
 
-                  suffix: Icon(Icons.analytics_outlined),
-                ),
-              ),
-            ),
-            Container(
-              width: 300,
-              child: TextField(
-                decoration: InputDecoration(
-                  hint: Text(
-                    'INPUT YOU passsword HERE',
-                    style: TextStyle(
-                      color: const Color.fromARGB(255, 39, 182, 44),
-                      fontWeight: FontWeight.bold,
-                    ),
-                  ),
-                  focusedBorder: OutlineInputBorder(
-                    borderRadius: BorderRadius.circular(14),
-                    borderSide: BorderSide(
-                      color: const Color.fromARGB(255, 44, 10, 101),
-                      width: 2,
-                    ),
-                  ),
-                ),
-              ),
-            ),
-            ElevatedButton(
-              onPressed: () {
-                String Uemail = email.text;
-              },
-              child: Text('Login'),
-            ),
+      //               suffix: Icon(Icons.analytics_outlined),
+      //             ),
+      //           ),
+      //         ),
+      //         Container(
+      //           width: 300,
+      //           child: TextField(
+      //             decoration: InputDecoration(
+      //               hint: Text(
+      //                 'INPUT YOU passsword HERE',
+      //                 style: TextStyle(
+      //                   color: const Color.fromARGB(255, 39, 182, 44),
+      //                   fontWeight: FontWeight.bold,
+      //                 ),
+      //               ),
+      //               focusedBorder: OutlineInputBorder(
+      //                 borderRadius: BorderRadius.circular(14),
+      //                 borderSide: BorderSide(
+      //                   color: const Color.fromARGB(255, 44, 10, 101),
+      //                   width: 2,
+      //                 ),
+      //               ),
+      //             ),
+      //           ),
+      //         ),
+      //         ElevatedButton(
+      //           onPressed: () {
+      //             String Uemail = email.text;
+      //           },
+      //           child: Text('Login'),
+      //         ),
+      //       ],
+      //     ),
+      //   ),
+      // body: Container(
+      //   child: Text('CURRNET TIME IS ,${DateFormat('QQQQ').format(Time)}'),
+      // ),
+      // body: Center(
+      //   child: Column(
+      //     mainAxisAlignment: MainAxisAlignment.center,
+      //     children: [
+      //       ElevatedButton(
+      //         onPressed: () async {
+      //           DateTime? datepicked = await showDatePicker(
+      //             context: context,
+      //             firstDate: DateTime(2021),
+      //             lastDate: DateTime(2025),
+      //           );
+      //         },
+      //         child: Text('press to see the date and time '),
+      //       ),
+      //       ElevatedButton(
+      //         onPressed: ()async {
+      //           TimeOfDay ?timepicked = await showTimePicker(
+      //             context: context,
+      //             initialTime: TimeOfDay.now(),
+      //           );
+      //         },
+      //         child: Text('time'),
+      //       ),
+      //     ],
+      //   ),
+      // ),
+      body: Container(
+        child: GridView.count(
+          crossAxisCount: 3,
+          children: [
+            Container(color: Colors.black),
+            Container(color: Colors.blue),
+            Container(color: Colors.green),
+            Container(color: Colors.cyanAccent),
+            Container(color: Colors.red),
+            Container(color: Colors.blueGrey),
+            Container(color: Colors.deepOrange),
+            Container(color: Colors.black),
           ],
         ),
       ),
